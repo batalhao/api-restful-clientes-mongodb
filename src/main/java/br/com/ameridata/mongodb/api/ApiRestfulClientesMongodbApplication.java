3d4bba7f1136369b50248562a524d2dce31090ff
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-15 08:59:52 -0300

    Primeira postagem do projeto API RESTful [Clientes/MongoDB]

---
+++ src/main/java/br/com/ameridata/mongodb/api/ApiRestfulClientesMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.ameridata.mongodb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulClientesMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulClientesMongodbApplication.class, args);
+	}
+
+}
